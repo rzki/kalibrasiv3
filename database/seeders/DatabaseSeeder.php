b665371c2c58018e1b5b5cf7da2c929072ff9293
Rizky Dhani <rizkydhani15@gmail.com>
2024-04-15 22:31:35 +0700

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Role;
+use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -34,12 +35,36 @@ class DatabaseSeeder extends Seeder
             'code' => 'teknisi',
         ]);
 
-        \App\Models\User::factory()->create([
+        User::create([
             'userId' => Str::orderedUuid(),
             'name' => 'Superadmin',
             'email' => 'superadmin@cal.medquest.co.id',
             'password' => Hash::make('Calibration24!'),
             'role_id' => 1
+        ]);
+
+        User::create([
+            'userId' => Str::orderedUuid(),
+            'name' => 'Muh Ardyansyah',
+            'email' => 'muh.ardyansyah@medquest.co.id',
+            'password' => Hash::make('Calibration24!'),
+            'role_id' => 2
+        ]);
+
+        User::create([
+            'userId' => Str::orderedUuid(),
+            'name' => 'Galuh Kurniawan',
+            'email' => 'galuh.kurniawan@medquest.co.id',
+            'password' => Hash::make('Calibration24!'),
+            'role_id' => 1
+        ]);
+
+        User::create([
+            'userId' => Str::orderedUuid(),
+            'name' => 'Admin',
+            'email' => 'admin@medquest.co.id',
+            'password' => Hash::make('Calibration24!'),
+            'role_id' => 2
         ]);
     }
 }
