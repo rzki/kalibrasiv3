e3bb9cd5d03ae7267cdd7dcc9eb719aa0e71f6f1
Rizky Dhani <rizkydhani15@gmail.com>
2024-04-23 15:55:41 +0700

    minor update hospitals #1

--- resources/views/hospitals/index.blade.php
+++ resources/views/hospitals/index.blade.php
@@ -21,8 +21,8 @@
                 <tr class="text-center">
                     <th scope="col">No</th>
                     <th scope="col">Name</th>
-                    <th scope="col">Phone Number</th>
                     <th scope="col">Address</th>
+                    <th scope="col">Phone Number</th>
                     <th scope="col">Action</th>
                 </tr>
             </thead>
